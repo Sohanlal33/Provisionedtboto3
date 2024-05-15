ef0ecbd57e4ad8f32b2f8e36730a9c6806c2bc44
Sohanlal33 <sohanlalchaudhary10@gmail.com>
2024-05-15 16:45:19 +0530

    Scheduled tasks in python--every 5 seconds

--- boton5.py
+++ boton5.py
@@ -2,6 +2,7 @@
 
 #created 3 ec2 instance through terraform 
 import boto3
+import schedule        #library added for scheduled tasks
 
 #for region 
 import boto3
@@ -17,9 +18,17 @@ for reservation in reservations['Reservations']:
     for instance in reservation['Instances']:
         print(f"Instance {instance['InstanceId']} is {instance['State']['Name']}")
 
-statuses = ec2_client.describe_instance_status()
+statuses = ec2_client.describe_instance_status(
+    IncludeAllInstances=True
+)
 for status in statuses ['InstanceStatuses']:
     ins_status = status['InstanceStatus']['Status']
     sys_status = status['SystemStatus']['Status']
     state =status['InstanceState']['Name']
-    print(f"Instance {status['InstanceId']} is {status} with instance status {ins-status} and system status  {sys status}")
+    print(f"Instance {status['InstanceId']} is {status} with instance status {ins-status} and system status {sys_status}")
+    print("#########################################\n") 
+
+schedule.every(5).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
