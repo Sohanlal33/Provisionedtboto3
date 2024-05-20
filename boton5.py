c17bf56b86fcc39698db70940a9593cb042da7ac
Sohanlal Chaudhary <163651175+Sohanlal33@users.noreply.github.com>
2024-05-20 18:36:50 +0530

    Update boton5.py

--- boton5.py
+++ boton5.py
@@ -4,9 +4,6 @@
 import boto3
 import schedule        #library added for scheduled tasks
 
-#for region 
-import boto3
-
 # for region
 ec2_client = boto3.client('ec2', region_name="us-east-1") 
 ec2_resource = boto3.resource('ec2', region_name="us-east-1")
